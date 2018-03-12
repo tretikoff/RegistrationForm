f5f0082cf95f123321d4e87db9c480ec7f903763
unknown <tretikoff@list.ru>
2018-03-12 11:30:35 +0300

    Initial commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+//using Qoden.UI;
+
+namespace Droid
+{
+    [Activity(Label = "Registration", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            
+            SetContentView (new Button(this));
+        }
+    }
+}
---
+++ Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Droid.MainApplication, Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5767c6430f12eaa2bb79d098a14927c03.MainApplication.class, md5767c6430f12eaa2bb79d098a14927c03.MainApplication.__md_methods);
+		
+	}
+}
